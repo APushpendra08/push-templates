ff460b41f11960cabd17f1a8395590c78de98ff2
A Pushpendra <aladupushpendra08@gmail.com>
2024-12-13 22:16:19 +0530

    Android 15 support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.al.play.pushtemplates"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.al.play.pushtemplates"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
